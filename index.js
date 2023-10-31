d45f5f82dbb7ed800394ea74579e8d5a1950fd6b
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-31 16:59:11 -0300

    feat: add js reference for the buttons
    I think... there are better ways to do this haha. Nonetheless, there are all the variables needed that come from the calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+let buttons = {
+  zero: document.querySelector("#zero").textContent ,
+  one: document.querySelector("#one").textContent ,
+  two: document.querySelector("#two").textContent ,
+  three: document.querySelector("#three").textContent ,
+  four: document.querySelector("#four").textContent ,
+  five: document.querySelector("#five").textContent ,
+  six: document.querySelector("#six").textContent ,
+  seven: document.querySelector("#seven").textContent ,
+  eight: document.querySelector("#eight").textContent ,
+  nine: document.querySelector("#nine").textContent ,
+  equal: document.querySelector("#equal").textContent ,
+  plus: document.querySelector("#plus").textContent ,
+  minus: document.querySelector("#minus").textContent ,
+  multiply: document.querySelector("#multiply").textContent ,
+  divide: document.querySelector("#divide").textContent ,
+  allclear: document.querySelector("#allclear").textContent ,
+}
+
+console.log(buttons)

fc249dbdf948d62e25887b76554ed2cc6ca740c8
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-11-01 18:09:47 -0300

    feat: remove collision in the display

--- index.js
+++ index.js
@@ -15,34 +15,45 @@ const operation = document.querySelector(".operation");
 
 function changeDisplay() {
   display.textContent = displayValue;
+  if (displayValue.length > 9) {
+    display.textContent = displayValue.substring(0, 9);
+  }
 }
 
 changeDisplay();
 
 function changeOperationDisplay() {
-  operation.textContent = `${firstValue} ${firstOperatorSymbol} ${secondValue}`
+
+    operation.textContent = operationValue;
+  
 }
 
-
+changeOperationDisplay();
 
 numbers.forEach(number => number.addEventListener("click", () => {
 
   if (firstValue === null) {
     if (displayValue === "0" || displayValue === 0) {
       displayValue = number.textContent;
+      operationValue = displayValue;
     } else if (displayValue === firstValue) {
       displayValue = number.textContent;
+      operationValue = displayValue;
     } else {
       displayValue += number.textContent;
+      operationValue = displayValue;
     } 
   } else {
     if (displayValue === firstValue) {
       displayValue = number.textContent;
+      operationValue = displayValue;
     } else {
       displayValue += number.textContent;
+      operationValue = displayValue;
     }
   }
 
+  changeOperationDisplay();
   changeDisplay()
 }))
 
@@ -53,6 +64,7 @@ operators.forEach(operator => operator.addEventListener("click", () => {
     secondValue = displayValue;
     result = calculate(firstOperatorSymbol, firstValue, secondValue);
     displayValue = result;
+    operationValue = `${firstValue} ${firstOperatorSymbol} ${secondValue}`
     firstValue = displayValue;
     result = null;
   } else if (firstOperatorSymbol != null && secondOperatorSymbol != null) {
@@ -60,37 +72,51 @@ operators.forEach(operator => operator.addEventListener("click", () => {
     result = calculate(secondOperatorSymbol, firstValue, secondValue);
     secondOperatorSymbol = operator.textContent;
     displayValue = result;
+    operationValue = `${firstValue} ${secondOperatorSymbol} ${secondValue}`
     firstValue = displayValue;
     result = null;
   } else {
     firstOperatorSymbol = operator.textContent;
     firstValue = displayValue;
+    if (secondValue != null) {
+      operationValue = `${firstValue} ${firstOperatorSymbol} ${secondValue}`
+    } else {
+      operationValue = `${firstValue} ${firstOperatorSymbol}`
+    }
   }
+
+  changeOperationDisplay();
 }))
 
 equalBtn.addEventListener("click", () => {
   if (firstOperatorSymbol === null) {
     displayValue = displayValue;
+    operationValue = displayValue;
   } else if (secondOperatorSymbol != null) {
     secondValue = displayValue;
     result = calculate(secondOperatorSymbol, firstValue, secondValue);
     if (result === "ErroR") {
       displayValue = "ErroR";
+      operationValue = `ErroR`
     } else {
       displayValue = result;
+      operationValue = `${firstValue} ${secondOperatorSymbol} ${secondValue}`
       firstValue = displayValue;
       secondValue = null;
       firstOperatorSymbol = null;
       secondOperatorSymbol = null;
       result = null;
+
     }
   } else {
     secondValue = displayValue;
     result = calculate(firstOperatorSymbol, firstValue, secondValue)
     if (result === "ErroR") {
       displayValue = "ErroR"
+      operationValue = `ErroR`
     } else {
       displayValue = result;
+      operationValue = `${firstValue} ${firstOperatorSymbol} ${secondValue}`
       firstValue = displayValue;
       secondValue = null;
       firstOperatorSymbol = null;
@@ -98,17 +124,20 @@ equalBtn.addEventListener("click", () => {
       result = null;
     }
   }
+  changeOperationDisplay();
   changeDisplay();
 })
 
 clearAll.addEventListener("click", () => {
   displayValue = "0";
+  operationValue = "0";
   firstValue = null;
   secondValue = null;
   firstOperatorSymbol = null;
   secondOpertarSymbol = null
   result = null;
   changeDisplay();
+  changeOperationDisplay();
 })
 
 function add(a, b) {

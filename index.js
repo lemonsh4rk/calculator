e86c2101a3793d10c2fa3e2f9454989b0b8021e9
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-11-01 13:14:55 -0300

    feat: add listeners for numbers and clearAll
    I need these like, obligated mostly, i'll deal with the order later

--- index.js
+++ index.js
@@ -9,33 +9,80 @@ let clearAll = document.querySelector("#allclear");
 let equalBtn = document.querySelector("#equal")
 let operation = document.querySelector(".operation");
 
+// number listener
+numbers.forEach(number => number.addEventListener("click", () => {
+
+  display.textContent += number.textContent;
+
+}))
+
+
+
+
+// this goes somewhere, idk
+result = calculate(operator, firstValue, secondValue)
+
+
+
+
+// clearall listener
+clearAll.addEventListener("click", () => {
+  firstValue = "";
+  secondValue = "";
+  operator = "";
+  result = null;
+  display.textContent = "";
+  operation.textContent = "";
+})
+
+
+
+
+
 
 
 function add(a, b) {
   return parseFloat(a) + parseFloat(b);
 };
+
 function substract(a, b) {
   return parseFloat(a) - parseFloat(b);
 };
+
 function multiply(a, b) {
   return parseFloat(a) * parseFloat(b);
 };
+
 function divide(a, b) {
   return parseFloat(a) / parseFloat(b);
 };
 
-function operate(operator, numberA, numberB) {
+function calculate(operator, numberA, numberB) {
   Number(numberA), Number(numberB);
 
   if (operator === "+") {
+
     return add(numberA, numberB);
+
   } else if (operator === "-") {
+
     return substract(numberA, numberB);
+
   } else if (operator === "*") {
+
     return multiply(numberA, numberB);
+
   } else if (operator === "/") {
+
+    if (numberA == 0) {
+      return `Can't do that`
+    }
+
     return divide(numberA, numberB);
+
   } else {
+
     return null
+
   }
 };

b8e46d4e621bf6c20ad3bff6a2b4c14f0eab7b28
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-11-01 13:01:21 -0300

    feat: create testing branch
    today i'll try to make something different i guess, starting from kind of scratch in the javascript side, so i can go with a clear mind

--- index.js
+++ index.js
@@ -1,66 +1,13 @@
 let firstValue = "";
 let secondValue = "";
 let operator = "";
-let result = "";
+let result = null;
 let numbers = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator")
 let display = document.querySelector("#display");
 let clearAll = document.querySelector("#allclear");
 let equalBtn = document.querySelector("#equal")
 let operation = document.querySelector(".operation");
-
-display.textContent = "";
-operation.textContent = "";
-
-
-numbers.forEach((button) => {
-  button.addEventListener("click", () => {
-
-    // operation.textContent = `${firstValue} ${operator} ${secondValue} = ${result}`;
-
-    display.textContent += button.textContent;
-  
-
-    operators.forEach((button) => {
-      button.addEventListener("click", () => {
-    
-        operator = button.textContent;
-    
-        if (firstValue == "" && secondValue == "") {
-          firstValue = display.textContent
-        } else if (firstValue != "" && secondValue == "") {
-          secondValue = display.textContent;
-          result = operate(operator, firstValue, secondValue);
-        } else if (firstValue != "" && secondValue != "") {
-          secondValue = firstValue;
-          firstValue = result;
-          result = result = operate(operator, firstValue, secondValue);
-        }
-    
-        display.textContent = "";
-        operation.textContent = `${firstValue} ${operator} ${secondValue} = ${result}`;
-        console.log(firstValue, secondValue, operator, result)
-      
-      })
-        
-      equalBtn.addEventListener("click", () => {
-
-          if (firstValue == "" || secondValue == "") {
-          }
-        })
-    
-      clearAll.addEventListener("click", () => {
-          firstValue = ""
-          secondValue = ""
-          operator = "";
-          display.textContent = "";
-          operation.textContent = "";
-        }) // I love this button so much, tysm ;-;
-      
-    });
-  })
-});
-
 
 
 
@@ -89,6 +36,6 @@ function operate(operator, numberA, numberB) {
   } else if (operator === "/") {
     return divide(numberA, numberB);
   } else {
-    return ""
+    return null
   }
 };
